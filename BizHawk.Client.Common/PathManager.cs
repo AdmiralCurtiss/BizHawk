485aa8ea7e2b40a8c62f9fe8a6bcb426bbfa48f5
Admiral H. Curtiss <pikachu025@gmail.com>
2016-08-14 20:45:45 +0200

    Libretro saves/states should not have a subdirectory, but instead handle similar to the multiple-cores-for-same-system name logic.
    This is so we can have Snes9x and BSNES saves in the same directory.

--- BizHawk.Client.Common/PathManager.cs
+++ BizHawk.Client.Common/PathManager.cs
@@ -294,17 +294,7 @@ namespace BizHawk.Client.Common
 
 		public static string RetroSaveRAMDirectory(GameInfo game)
 		{
-			var name = RemoveInvalidFileSystemChars(game.LibretroCore);
-
-			if (Global.MovieSession.Movie.IsActive)
-			{
-				name = Path.Combine(name, "movie-" + Path.GetFileNameWithoutExtension(Global.MovieSession.Movie.Filename));
-			}
-
-			var pathEntry = Global.Config.PathEntries[game.System, "Save RAM"] ??
-							Global.Config.PathEntries[game.System, "Base"];
-
-			return Path.Combine(MakeAbsolutePath(pathEntry.Path, game.System), name);
+			return SaveRamPath(game);
 		}
 
 
@@ -358,6 +348,11 @@ namespace BizHawk.Client.Common
 			if (Global.MovieSession.Movie.IsActive)
 			{
 				name += "." + Path.GetFileNameWithoutExtension(Global.MovieSession.Movie.Filename);
+			}
+
+			if (game.System == "Libretro")
+			{
+				name += "." + game.LibretroCore;
 			}
 
 			var pathEntry = Global.Config.PathEntries[game.System, "Savestates"] ??
