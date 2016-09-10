79094179e096a8f8bf2638a3608406eda0c069a6
Admiral H. Curtiss <pikachu025@gmail.com>
2016-09-10 19:42:48 +0200

    Update release date.

--- Version/VersionInfo.cs
+++ Version/VersionInfo.cs
@@ -1,7 +1,7 @@
 static class VersionInfo
 {
 	public const string MAINVERSION = "1.11.7"; // Use numbers only or the new version notification won't work
-	public static readonly string RELEASEDATE = "September 1, 2016";
+	public static readonly string RELEASEDATE = "2016-09-10";
 	public static readonly bool DeveloperBuild = false;
 	public static readonly string HomePage = "http://tasvideos.org/BizHawk.html";
 
